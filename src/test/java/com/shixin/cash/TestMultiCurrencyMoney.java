31d4aadadab9d104acdc4a8fe32e03dc152b8710
shixin <weizhiwen23@gmail.com>
2024-03-16 17:22:09 +0800

    chapter 4 privacy

--- src/test/java/com/shixin/cash/TestMultiCurrencyMoney.java
+++ src/test/java/com/shixin/cash/TestMultiCurrencyMoney.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * $5 + 10CHF = $10 if rate is 2:1 <br>
  * <s>$5 * 2 = $10</s> <br>
- * Make "amount" private <br>
+ * <s>Make "amount" private</s> <br>
  * <s>Dollar side-effects?</s> <br>
  * Money rounding? <br>
  * <s>equals()</s> <br>
@@ -20,10 +20,8 @@ class TestMultiCurrencyMoney {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
