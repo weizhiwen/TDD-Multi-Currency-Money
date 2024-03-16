612dd614bebe363b809bf2d6aa7c55b669dbff3d
shixin <weizhiwen23@gmail.com>
2024-03-16 17:08:22 +0800

    chapter 3

--- src/test/java/com/shixin/cash/TestMultiCurrencyMoney.java
+++ src/test/java/com/shixin/cash/TestMultiCurrencyMoney.java
@@ -2,14 +2,18 @@ package com.shixin.cash;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * $5 + 10CHF = $10 if rate is 2:1 <br>
  * <s>$5 * 2 = $10</s> <br>
  * Make "amount" private <br>
  * <s>Dollar side-effects?</s> <br>
- * Money rounding <br>
+ * Money rounding? <br>
+ * <s>equals()</s> <br>
+ * hashCode() <br>
+ * Equal null <br>
+ * Equal Object <br>
  */
 class TestMultiCurrencyMoney {
 
@@ -20,5 +24,11 @@ class TestMultiCurrencyMoney {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
