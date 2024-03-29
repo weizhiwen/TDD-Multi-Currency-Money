49ab56673a8ffff920c68670118cc2ccca3a65e4
shixin <763107756@qq.com>
2024-03-29 16:54:26 +0800

    chapter 14

--- src/main/java/com/shixin/cash/Expression.java
+++ src/main/java/com/shixin/cash/Expression.java
@@ -1,5 +1,5 @@
 package com.shixin.cash;
 
 public interface Expression {
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 }
--- src/main/java/com/shixin/cash/Sum.java
+++ src/main/java/com/shixin/cash/Sum.java
@@ -11,7 +11,7 @@ public class Sum implements Expression {
     }
 
     @Override
-    public Money reduce(String to) {
+    public Money reduce(Bank bank, String to) {
         return new Money(augend.amount + addend.amount, to);
     }
 }
